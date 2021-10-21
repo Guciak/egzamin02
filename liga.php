720a77a9b528c52a1575947861a198efabef7cd3
Guciak <patrykgut08@gmail.com>
2021-10-21 12:58:46 +0200

    pierwsze

--- liga.php
+++ liga.php
@@ -1,26 +1,47 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>piłka nożna</title>
-    <link rel="stylsheet" href="styl2.css"
-
+    <title>Piłka nożna</title>
+    <link rel="stylesheet" href="styl2.css">
 </head>
+
 <body>
-
-
-<header id="banner">
-<h3>Reprezentacja polski w piłce nożnej</h3>
+    <div class="baner">
+        <h3>Reprezentacja Polski w piłce nożnej</h3>
         <img src="obraz1.jpg" alt="reprezentacja">
-</header>
-<section id="lewa" >
-</section> 
+    </div>
 
-<section id="prawa"></section>
-    <main id="glowny"></main>
-    <section id="liga"></section>
-    
+
+    <main>
+        <div class="lewy">
+            <form action="liga.php" method="POST">
+                <select name="lista" id="">
+                    <option value="1" selected>Bramkarze</option>
+                    <option value="2">Obrońcy</option>
+                    <option value="3">Pomocnicy</option>
+                    <option value="4">Napastnicy</option>
+                </select>
+
+                <button type="submit" name="zobacz">ZOBACZ</button>
+
+            </form>
+
+            <img src="zad2.png" alt="piłka">
+            <br><br>
+            <p>Autor: Patryk Gut 4TIa</p>
+        </div>
+        <div class="prawy">
+        </div>
+    </main>
+    <h3>Liga Mistrzów</h3>
+    </div>
+
 </body>
+
 </html>
+
+
